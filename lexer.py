cd9bda6c3e53be89b9c70b0973c6e0f5a91f805a
Leandro Silva <jlsf@ic.ufal.br>
2018-03-14 15:26:43 -0300

    Finished the tokenizer and the specification of the tokens

--- lexer.py
+++ lexer.py
@@ -14,39 +14,101 @@ class Token():
 class Tokenizer():
 
     def __init__(self, tokens, categories, target):
-        self.target = target
+        
         self.tokens = tokens
         self.categories = categories
-        print('|'.join('(P<%s>%s)' % token for token in tokens))
-        self.regex = re.compile('|'.join('(?P<%s>%s)' % token for token in tokens))
         self.pos = 0
+        
+        self.regex = re.compile('|'.join('(?P<%s>%s)' % token for token in tokens))
+        self.target = re.sub(re.compile('//.*?\n'), '', target)[:-1]
+        self.ws_skip = re.compile('\s')
 
-        self.ws_skip = re.compile('\S')
-
+    def hasToken(self):
+        return False if self.pos >= len(self.target) else True
+        
     def nextToken(self):
 #TODO
 # Decide if some of these are really necessary
-        w = self.ws_skip.match(self.target, self.pos)
-        if w:
-            self.pos = w.start()
-        else:
-            return None
-        
+        ws = self.ws_skip.match(self.target, self.pos)
+        if ws:
+            self.pos += 1
+            
         m = self.regex.match(self.target, self.pos)
-
         if m:
             category_id = m.lastgroup
             category_num = self.categories[category_id]
-            new_token = Token((self.pos, 1), (category_num, category_id), m.string)
-
+            new_token = Token((self.pos, 1), (category_num, category_id), m.string[m.start():m.end()])
+            self.pos = m.end()
             return new_token
 
-        return 0
+        self.pos = len(self.target)
+        return 'error'
         
 if __name__ == '__main__':
 
-    tokens = [('id', '\w+')]
-    categories = {'id': 1}
+    tokens = [
+        ('KWMAIN', '(main)(?!\w)'),
+        ('KWIO', '(input|output)(?!\w)'),
+        ('KWIF', '(if)(?!\w)'),
+        ('KWELSE', '(else)(?!\w)'),
+        ('KWFOR', '(for)(?!\w)'),
+        ('KWWHILE', '(while)(?!\w)'),
+        ('KWRANGE', '(range)(?!\w)'),
+        ('PTYPE', '(string|int|float|bool)(?!\w)'),
+        ('CTEFLOAT', '[+-]?\d+\.\d+(?!\w)'),
+        ('CTEINT', '[+-]?\d+(?!\w)'),
+        ('LTBOOL', '(true|false)(?!\w)'),
+        ('OPCMP', '>=|>|<=|<|==|!='),
+        ('OPAT', '='),
+        ('OPMBR', '(in)(?!\w)'),
+        ('OPAD', '\+|-'),
+        ('OPML', '\*|/|%'),
+        ('OPCONJ', '(?<!\w)(and)(?!\w)'),
+        ('OPDISJ', '(or)(?!\w)'),
+        ('OPNEG', '(not)(?!\w)'),
+        ('ST', ';'),
+        ('CLN', ','),
+        ('PARST', '\('),
+        ('SQBRST', '\['),
+        ('CLBRST', '\{'),
+        ('PAREND', '\)'),
+        ('SQBREND', '\]'),
+        ('CLBREND', '\}'),
+        ('ID', '[a-zA-Z_]+[0-9]*[a-zA-Z_]*'),
+    ]
+    categories = {
+        'KWMAIN': 1,
+        'KWIO': 2,
+        'KWIF': 3,
+        'KWELSE': 4,
+        'KWFOR': 5,
+        'KWWHILE': 6,
+        'KWRANGE': 7,
+        'PTYPE': 8,
+        'CTEFLOAT': 9,
+        'CTEINT': 10,
+        'LTBOOL': 11,
+        'OPCMP': 12,
+        'OPAT': 13,
+        'OPMBR': 14,
+        'OPAD': 15,
+        'OPML': 16,
+        'OPCONJ': 17,
+        'OPDISJ': 18,
+        'OPNEG': 19,
+        'ST': 20,
+        'CLN': 21,
+        'PARST': 22,
+        'SQBRST': 23,
+        'CLBRST': 24,
+        'PAREND': 25,
+        'SQBREND': 26,
+        'CLBREND': 27,
+        'ID': 28
+    }
 
-    tokenizer = Tokenizer(tokens, categories, '125')
-    print(tokenizer.nextToken())
+    tokenizer = Tokenizer(tokens, categories, 'main range -125.0; not }{12 //and a] % 2;\nmain\n')
+
+    while tokenizer.hasToken():
+        print(tokenizer.nextToken()) 
+    
